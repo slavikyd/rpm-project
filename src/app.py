4e72b4732e5e6d2fdd298e2e7bf27f6132b5298b
Vadim Nikiforov <vadim.nikiforov.2017@gmail.com>
2024-10-18 22:07:03 +0000

    create logging

--- src/app.py
+++ src/app.py
@@ -1,9 +1,20 @@
+import asyncio
 import uvicorn
+
 from fastapi import FastAPI
+from logger import logger
+
 
 def create_app() -> FastAPI:
     app = FastAPI()
     return app
 
+
+async def start_pooling() -> None:
+    logger.info('Starting pooling..')
+    await asyncio.sleep(100)
+
+
 if __name__ == '__main__':
-    uvicorn.run('src.app:create_app', factory=True, host='0.0.0.0', port=8000, workers=1)
+    # uvicorn.run('src.app:create_app', factory=True, host='0.0.0.0', port=8000, workers=1)
+    asyncio.run(start_pooling())
