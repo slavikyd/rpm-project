ad0e068f9037263b6c243a10134462dec83ce319
Vadim Nikiforov <vadim.nikiforov.2017@gmail.com>
2024-10-20 10:56:04 +0000

    add webhooks

--- src/app.py
+++ src/app.py
@@ -1,20 +1,38 @@
 import asyncio
 import uvicorn
-
+from aiogram import Dispatcher, Bot
 from fastapi import FastAPI
-from logger import logger
+from src.api.tg.router import router as tg_router
+from starlette_context import plugins
+from starlette_context.middleware import RawContextMiddleware
+
+dp = Dispatcher()
+background_tasks = set()
+
+TOKEN = ''
+WEBHOOK_URL = ''
+
+async def lifespan(app: FastAPI):
+
+    dp = Dispatcher()
+    bot = Bot(token=TOKEN)
+
+    temp = await bot.get_webhook_info()
+
+    await bot.set_webhook(WEBHOOK_URL)
+
+    yield
+    while background_tasks:
+        await asyncio.sleep(0)
 
 
 def create_app() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(docs_url='/swagger', lifespan=lifespan)
+    app.include_router(tg_router, prefix='/tg', tags=['tg'])
+
+    app.add_middleware(RawContextMiddleware, plugins=[plugins.CorrelationIdPlugin()])
     return app
 
 
-async def start_pooling() -> None:
-    logger.info('Starting pooling..')
-    await asyncio.sleep(100)
-
-
 if __name__ == '__main__':
-    # uvicorn.run('src.app:create_app', factory=True, host='0.0.0.0', port=8000, workers=1)
-    asyncio.run(start_pooling())
+    uvicorn.run('src.app:create_app', factory=True, host='0.0.0.0', port=5783, workers=1)
