f7b21de932b91b0b050ecfaf804cd7bc8bc271eb
slavikyd <slavikyd@gmail.com>
2024-11-22 09:10:09 +0300

    form message handler fixed, typoos fix and minor changes

--- consumer/handlers/form.py
+++ consumer/handlers/form.py
@@ -1,7 +1,8 @@
 import aio_pika
 import msgpack
+# import logging
 from aio_pika import ExchangeType
-from sqlalchemy import select, func
+from sqlalchemy import select, func, insert
 
 from config.settings import settings
 from consumer.model.user import User
@@ -14,27 +15,17 @@ async def handle_event_form(message: FormMessage):
     if message['action'] == 'send_form':
         # TODO: отправить данные из сооющения в БД
         async with async_session() as db:
-
-            not_fetched = await db.execute(select(User).order_by(func.random()))
-            tuple_rows = not_fetched.all()
-            users = [row for row, in tuple_rows]
-
-            async with channel_pool.acquire() as channel:  # type: aio_pika.Channel
-                exchange = await channel.declare_exchange("users", ExchangeType.TOPIC, durable=True)
-
-                for user in users:
-                    await exchange.publish(
-                        aio_pika.Message(
-                            msgpack.packb({
-                                'name': user.name,
-                                'age': user.age,
-                                'gender': user.gender,
-                                'description': user.description,
-                                'filter_by_age': user.filter_by_age,
-                                'filter_by_gender': user.filter_by_gender,
-                                'filter_by_description': user.filter_by_description,
-                            }),
-                        ),
-                        routing_key=settings.USER_RECOMMENDATIONS_QUEUE_TEMPLATE.format(user_id=message['user_id']),
-                    )
-        print(message)
+            # TODO: УБРАТЬ ЭТУ ДИЧЬ
+            user_data = {
+                'user_id': message.user_id,
+                'name': message.name,
+                'age': message.age,
+                'gender': message.gender,
+                'description': message.description,
+                'filter_by_age': message.filter_by_age,
+                'filter_by_gender': message.filter_by_gender,
+                'filter_by_description': message.filter_by_description,
+            }
+            await db.execute(insert(User).values(**user_data))
+            print(message)
+        # logging.info(message)
