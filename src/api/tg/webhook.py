b35b66847153d65f333750b8fe5a9e93fa07b43c
EgorikA4 <egorik4litvinov@mail.ru>
2024-11-29 22:51:51 +0300

    fixed webhook issues

--- src/api/tg/webhook.py
+++ src/api/tg/webhook.py
@@ -8,15 +8,18 @@ from starlette.requests import Request
 from starlette.responses import JSONResponse
 
 from src import bot
-from src.api.tg.router import router
 from src.bg_tasks import background_tasks
+from src.api.tg.router import router
 
 
+@router.post('/webhook')
 async def webhook(request: Request) -> JSONResponse:
     update = await request.json()
 
-    task: Task[TelegramMethod[Any] | None] = asyncio.create_task(bot.dp.feed_webhook_update(bot.bot, update))
+    task: Task[TelegramMethod[Any] | None] = asyncio.create_task(
+        bot.dp.feed_webhook_update(bot.bot, update),
+    )
     background_tasks.add(task)
-    tasks.add_done_callback(background_tasks.discard)
+    task.add_done_callback(background_tasks.discard)
 
     return ORJSONResponse({'status': 'ok'})
